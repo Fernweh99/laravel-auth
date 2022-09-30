4a7237a6f7ff863f8207ee56b666e63839fd4d45
Fernweh99 <daniele.dimento@gmail.com>
2022-09-30 16:25:09 +0200

    set form with check and fix postCon x it

--- database/migrations/2022_09_30_121248_create_post_tag_table.php
+++ database/migrations/2022_09_30_121248_create_post_tag_table.php
@@ -17,10 +17,10 @@ class CreatePostTagTable extends Migration
             $table->id();
 
             $table->unsignedBigInteger('post_id');
-            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
 
             $table->unsignedBigInteger('tag_id');
-            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
 
             $table->timestamps();
         });
