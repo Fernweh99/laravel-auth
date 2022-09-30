9ef40600da4c032191445400503ca7818e3746ac
Fernweh99 <daniele.dimento@gmail.com>
2022-09-30 14:11:51 +0200

    add Tag Model & set Seeder & Migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             UserDetailSeeder::class,
+            TagSeeder::class,
             CategorySeeder::class,
             PostSeeder::class,
         ]);
